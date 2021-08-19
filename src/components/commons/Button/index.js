50d3a6a7f1ceae8ef9e4994b37512b206b38ae6a
shinjiaki <julio_shinji@hotmail.com>
2021-08-18 23:38:46 -0300

    M1C6

--- src/components/commons/Button/index.js
+++ src/components/commons/Button/index.js
@@ -1,6 +1,7 @@
 import styled, { css } from 'styled-components'
 import get from 'lodash/get'
 import { TextStyleVariantsMap } from '../../foundation/Text';
+import { breakpointsMedia } from '../../../theme/utils/breakpointsMedia';
 
 const ButtonGhost = css`
   color: ${(props) => get(props.theme, `colors.${props.variant}.color`)};
@@ -31,4 +32,13 @@ export const Button = styled.button`
   &:focus {
     opacity: .5;
   }
+
+  ${breakpointsMedia({
+    xs: css`
+      ${TextStyleVariantsMap.smallestException}
+    `,
+    md: css`
+      ${TextStyleVariantsMap.paragraph1}
+    `,
+  })}
 `;
