ae025125a53a93c65ca897c3ba859530386b2b66
shinjiaki <julio_shinji@hotmail.com>
2021-08-30 16:18:03 -0300

    M1C7

--- src/components/foundation/Text/index.js
+++ src/components/foundation/Text/index.js
@@ -19,11 +19,12 @@ const TextBase = styled.span`
   ${(props) => TextStyleVariantsMap[props.variant] }
 `;
 
-export default function Text({ tag, variant, children }) {
+export default function Text({ tag, variant, children, ...props }) {
   return (
     <TextBase
       as={tag}
       variant={variant}
+      {...props}
     >
       {children}
     </TextBase>
